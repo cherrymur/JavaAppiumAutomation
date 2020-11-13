23b9151b9796028bb330d0cc4ad5ae3692a74284
Olga Avramenko <olga.avramenko@gs-labs.ru>
2020-11-13 18:31:25 +0300

    Module1_lesson10_Private_methods created

--- src/MathHelper.java
+++ src/MathHelper.java
@@ -1,12 +1,45 @@
 public class MathHelper
 {
-    public int multiply(int number)
-    {
-        return number * 5;
+    public int calc(int a, int b, char action) {
+        if (action == '+') {
+            return this.plus(a, b);
+        } else if (action == '-') {
+            return this.minus(a, b);
+        } else if (action == '/') {
+            return this.divide(a, b);
+        } else if (action == '*') {
+            return this.multiply(a, b);
+        } else {
+            return typeAnErrorAndReturnDefaultValue("Wrong action: " + action);
+        }
     }
 
-    public int multiply(int number, int multiplier)
+    private int typeAnErrorAndReturnDefaultValue (String error_message) {
+        System.out.println(error_message);
+        return 0;
+    }
+
+    private int plus(int a, int b)
     {
-        return number * multiplier;
+        return a + b;
+    }
+
+    private int minus(int a, int b)
+    {
+        return a - b;
+    }
+
+    private int multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    private int divide(int number, int divider)
+    {
+        if(divider == 0) {
+            return this.typeAnErrorAndReturnDefaultValue("Cannot divide by zero");
+        } else {
+            return number / divider;
+        }
     }
 }
