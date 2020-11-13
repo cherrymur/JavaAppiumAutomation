75b3469bd48a26b8debdcea7ea2ff112635e8133
Olga Avramenko <olga.avramenko@gs-labs.ru>
2020-11-13 18:03:44 +0300

    Module1_lesson9_Parent_class_created

---
+++ src/CoreTestCase.java
@@ -0,0 +1,12 @@
+public class CoreTestCase
+{
+    public int multiply(int number)
+    {
+        return number * 5;
+    }
+
+    public int multiply(int number, int multiplier)
+    {
+        return number * multiplier;
+    }
+}
--- src/MainTest.java
+++ src/MainTest.java
@@ -1,10 +1,9 @@
 import org.junit.Test;
 
-public class MainTest
+public class MainTest extends CoreTestCase
 {
     int a = 5;
     int b = 11;
-
 
     @Test
     public void myFirstTest()
@@ -14,21 +13,6 @@ public class MainTest
 
         int b = this.multiply( 5, 2);
         System.out.println(b);
-        if(a > b) {
-            System.out.println("False");
-        } else {
-            System.out.println("True");
-        }
-    }
-
-    public int multiply(int number)
-    {
-        return number * 5;
-    }
-
-    public int multiply(int number, int multiplier)
-    {
-        return number * multiplier;
     }
 }
 
