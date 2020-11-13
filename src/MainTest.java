1492470be3c7267a7573242a4edce66f46cb83b5
Olga Avramenko <olga.avramenko@gs-labs.ru>
2020-11-13 17:21:19 +0300

    Module1_lesson8_Methods

--- src/MainTest.java
+++ src/MainTest.java
@@ -9,8 +9,11 @@ public class MainTest
     @Test
     public void myFirstTest()
     {
-        int a = this.giveMeInt();
+        int a = this.multiply( 5);
+        System.out.println(a);
 
+        int b = this.multiply( 5, 2);
+        System.out.println(b);
         if(a > b) {
             System.out.println("False");
         } else {
@@ -18,9 +21,14 @@ public class MainTest
         }
     }
 
-    public int giveMeInt()
+    public int multiply(int number)
     {
-        return 5;
+        return number * 5;
+    }
+
+    public int multiply(int number, int multiplier)
+    {
+        return number * multiplier;
     }
 }
 
