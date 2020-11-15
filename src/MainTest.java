a132829b7f1e76b75a2fbbbb2f261e937219a51e
Olga Avramenko <olga.avramenko@gs-labs.ru>
2020-11-15 04:30:56 +0300

    Module1_lesson11_How_to_work_with_static_fields

--- src/MainTest.java
+++ src/MainTest.java
@@ -2,16 +2,28 @@ import org.junit.Test;
 
 public class MainTest extends CoreTestCase
 {
-    MathHelper Math = new MathHelper();
-
     @Test
     public void myFirstTest()
     {
-        int a = Math.multiply( 5);
-        System.out.println(a);
+        System.out.println("First test: Before changing static_int: " + MathHelper.static_int);
 
-        int b = Math.multiply( 5, 2);
-        System.out.println(b);
+        MathHelper.static_int = 8;
+        MathHelper mathObject = new MathHelper();
+
+        System.out.println("First test: Before changes simple_int: " + mathObject.simple_int);
+        mathObject.simple_int = 9;
+    }
+
+    @Test
+    public void mySecondTest()
+    {
+        System.out.println("Second test: Before changing static_int: " + MathHelper.static_int);
+
+        MathHelper.static_int = 8;
+        MathHelper mathObject = new MathHelper();
+
+        System.out.println("Second test: Before changes simple_int: " + mathObject.simple_int);
+        mathObject.simple_int = 9;
     }
 }
 
