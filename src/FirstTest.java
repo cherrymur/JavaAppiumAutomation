56bf080affb66307e923980dcff6885e50d5a5fb
Olga Avramenko <olga.avramenko@gs-labs.ru>
2020-11-17 13:32:25 +0300

    Module2_Method_tearDown_created_and_markers_added

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,6 +1,9 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.apache.xpath.operations.And;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
@@ -8,6 +11,7 @@ import java.net.URL;
 public class FirstTest {
     private AppiumDriver driver;
 
+    @Before
     public void setUp() throws Exception
     {
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -21,5 +25,17 @@ public class FirstTest {
                 "JavaAppiumAutomatiom\\apks\\org.wikipedia");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown()
+    {
+        driver.quit();
+    }
+
+    @Test
+    public void firstTest()
+    {
+        System.out.println("First test run");
     }
 }
