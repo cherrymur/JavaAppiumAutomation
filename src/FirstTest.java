35991b05fd0ffccec8f9c613475d941777f28979
Olga Avramenko <olga.avramenko@gs-labs.ru>
2020-11-17 13:28:30 +0300

    Module2_Method_setUp_created

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,3 +1,25 @@
-public class FirstTest
-{
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.apache.xpath.operations.And;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class FirstTest {
+    private AppiumDriver driver;
+
+    public void setUp() throws Exception
+    {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("PlatformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "C:\\Users\\olga.avramenko\\Desktop\\Mobile_testing\\" +
+                "JavaAppiumAutomatiom\\apks\\org.wikipedia");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
 }
